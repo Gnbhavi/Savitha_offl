2ae5eed6d70eac93713d4a4419720bba771d1e97
Bhavi <nallabhavitran@gmail.com>
2023-11-09 13:36:38 +0530

    python file

--- csv_files 9th_NOV/droping_companies_in_panel_data.py
+++ csv_files 9th_NOV/droping_companies_in_panel_data.py
@@ -2,18 +2,28 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv(
-    "~/Python_github/savitha/Savitha_offl/panel_data_predicting_esg_data__control_variable_filled.csv"
+    "~/Python_github/savitha/Savitha_offl/csv_files 9th_NOV/independent_dependent_control_variable_filled_except_levarage.csv"
 )
 df = pd.DataFrame(df)
 
 dropping_comp = pd.read_excel(
-    "~/Python_github/savitha/Savitha_offl/Drop_list_90_firms.xlsx"
+    "~/Python_github/savitha/Savitha_offl/csv_files 9th_NOV/Drop_list_90_firms.xlsx"
 )
 dropping_comp = pd.DataFrame(dropping_comp)
 dropping_comp.columns = dropping_comp.iloc[0]
 dropping_comp = dropping_comp.drop(dropping_comp.index[0])
 
 dropping_comp_list = dropping_comp["Housing Finance Company"]
-dropping_comp_list = dropping_comp_list[19:]
+dropping_comp_list = dropping_comp_list[19:].tolist()
 
-print(len(dropping_comp_list))
+df_after_dropping = df.copy()
+for companies in dropping_comp_list:
+    companyname = companies.replace(" IN Equity", "")
+    df_after_dropping = df_after_dropping[
+        df_after_dropping["Company Name"] != companyname
+    ]
+
+
+df_after_dropping.to_csv(
+    "~/Python_github/savitha/Savitha_offl/csv_files 9th_NOV/independent_dependent_control_variable_filled_except_levarage_companies_dropped.csv"
+)
