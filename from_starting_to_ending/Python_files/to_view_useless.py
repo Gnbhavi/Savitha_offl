9d8abd25f6064b9300b3873d3d9b2fe6c34c6ed6
Bhavi <nallabhavitran@gmail.com>
2024-02-01 07:32:55 +0530

    1 st feb

--- from_starting_to_ending/Python_files/to_view_useless.py
+++ from_starting_to_ending/Python_files/to_view_useless.py
@@ -1,26 +1,13 @@
-import pickle
 import pandas as pd
 
-file_path = "/Users/gnbhavithran/Python_github/savitha/Savitha_offl/from_starting_to_ending/independent_variables_wasted_companies_dict.pkl"
-
-# open a file, where you stored the pickled data
-file = open(file_path, "rb")
-
-# dump information to that file
-wasted_companies = pickle.load(file)
-
-
-# close the file
-file.close()
-
 df = pd.read_csv(
-    "/Users/gnbhavithran/Python_github/savitha/Savitha_offl/from_starting_to_ending/dependent_control_variable_filled.csv"
+    "/Users/gnbhavithran/Python_github/savitha/Savitha_offl/from_starting_to_ending/new_files/1st_feb/dependent_control_variable_fully_filled_ADANIES_GDs_Below_100.csv",
+    index_col=None,
 )
 
-df_copy = df.copy()
-i = 0
-for val in wasted_companies:
-    if len(wasted_companies[val]) != 0:
-        i += 1
-        print(val)
-        print(f"{i}) {val} : {wasted_companies[val]}")
+
+df_1 = df[df["Year"] > 2008]
+
+df_1.to_csv(
+    "/Users/gnbhavithran/Python_github/savitha/Savitha_offl/from_starting_to_ending/new_files/1st_feb/dependent_control_variable_fully_filled_ADANIES_GDs_Below_100_lst_15_years.csv"
+)
