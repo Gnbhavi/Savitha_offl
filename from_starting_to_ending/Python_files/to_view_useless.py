fbe253730d261b0e35f92cf5441bb6187ebdbc61
Bhavi <nallabhavitran@gmail.com>
2024-02-01 09:32:44 +0530

    hi

--- from_starting_to_ending/Python_files/to_view_useless.py
+++ from_starting_to_ending/Python_files/to_view_useless.py
@@ -1,13 +1,12 @@
 import pandas as pd
+from pandas.io.pytables import com
 
 df = pd.read_csv(
     "/Users/gnbhavithran/Python_github/savitha/Savitha_offl/from_starting_to_ending/new_files/1st_feb/dependent_control_variable_fully_filled_ADANIES_GDs_Below_100.csv",
     index_col=None,
 )
 
+comp_n = "MAXHEALT"
 
-df_1 = df[df["Year"] > 2008]
 
-df_1.to_csv(
-    "/Users/gnbhavithran/Python_github/savitha/Savitha_offl/from_starting_to_ending/new_files/1st_feb/dependent_control_variable_fully_filled_ADANIES_GDs_Below_100_lst_15_years.csv"
-)
+print(df.loc[df["Company Name"] == comp_n, "ALTMAN_Z_SCORE"])
