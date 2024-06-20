cbf893aa5aaf7c3d3fd4089506d4c48925ba7eb9
Bhavi <nallabhavitran@gmail.com>
2024-06-21 01:41:10 +0800

    samme

--- from_starting_to_ending/FOr_service_energy_and_Textile/textile/Python FIles/timelag.py
+++ from_starting_to_ending/FOr_service_energy_and_Textile/textile/Python FIles/timelag.py
@@ -9,3 +9,14 @@ from sklearn.linear_model import LinearRegression
 
 I_V = ['ESG_DISCLOSURE_SCORE', 'ENVIRON_DISCLOSURE_SCORE','SOCIAL_DISCLOSURE_SCORE', 'GOVNCE_DISCLOSURE_SCORE']
 
+df = pd.read_csv( "/Users/gnbhavithran/Python_github/savitha/Savitha_offl/from_starting_to_ending/FOr_service_energy_and_Textile/textile/Dataset_creation/Fully_filled_data_ready_for_use.csv",
+    parse_dates=["Year"],
+)
+df["Year"] = df['Year'].dt.year
+
+for values in I_V:
+  df[values] = df[values].shift(1)
+
+filtered_year_df = df[df['Year'] >= 2015]
+
+filtered_year_df.to_csv("/Users/gnbhavithran/Python_github/savitha/Savitha_offl/from_starting_to_ending/FOr_service_energy_and_Textile/textile/Dataset_creation/year_filtered_ready_for_model_training.csv")
